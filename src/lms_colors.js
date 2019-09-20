c82567f3e87be7b2c918ddfe9cc5aec46315cd42
jadedkitty <jadedkittycat@gmail.com>
2019-09-19 17:38:35 -0700

    Cleaned up code and removed deprecated functions
    Cleaned up code and removed deprecated detection functions

--- src/lms_colors.js
+++ src/lms_colors.js
@@ -2,38 +2,21 @@ var BackgroundColor = "#34363e";
 
 function setBlack(widget) {
   document.getElementById(widget).className = "tac-dashboard-box dashboard-box-black";
-  //document.getElementById("creditClassWrapper").className = "tac-dashboard-box dashboard-box-black";
-  //document.getElementById("studentSubmissionWrapper").className = "tac-dashboard-box dashboard-box-black";
-  //document.getElementById("studentGradebookWrapper").className = "tac-dashboard-box dashboard-box-black";
-  //document.getElementById("wrapper_campus_communities").className = "tac-dashboard-box dashboard-box-black";
-  //document.getElementById("wrapper_comp_based_courses2").className = "tac-dashboard-box dashboard-box-black";
-  //document.getElementById("wrapper_events_lecturers").className = "tac-dashboard-box dashboard-box-black";
-  //document.getElementById("planner_accordionBody_0").style.color = "#64686a";
 }
 
-// runs setblack and darkplanner 3.5 seconds after page load
-window.onload = function() {
+
+window.onload = function() {  // runs functions on page load for detecting widgets and setting dark colors
   main();
+  // detecting widgets
   detectWidget('wrapper_events_lecturers');
   detectWidget('creditClassWrapper');
   detectWidget('studentSubmissionWrapper');
   detectWidget('studentGradebookWrapper');
   detectWidget('wrapper_campus_communities');
   detectWidget('wrapper_comp_based_courses2');
-  
-  // class dashboard
-  //detectWidget('lc-hom-box');
-  
-  //planner text
-  plannerText();
-  //calendar();
-  //setTimeout(setBlack, 3500);
-  //setTimeout(darkPlanner, 3500);
 }
 
 function main() {
-  document.body.style.background = BackgroundColor;
-  document.getElementById("headerId").style.background = "#434653";
   document.getElementById("headerId").style.background = "#434653";
   document.getElementsByClassName('lc_navIcons dashboardIco')[0].setAttribute("style", 'background: url("https://i.imgur.com/RqBuY1S.png") no-repeat scroll 0 0 rgba(0,0,0,0)');
   document.getElementsByTagName('img')[0].setAttribute("src", "https://i.imgur.com/n50MHX2.png");
@@ -47,33 +30,11 @@ function main() {
   document.getElementById("selectedNavTitle").setAttribute("style", "color: #ffffff !important");
 }
 
-function darkPlanner() {
-  // document.getElementsByClassName('current')[0].setAttribute("style", "background: #218ae7!important");
-  // document.getElementsByClassName('ui-widget-content')[0].setAttribute("style", "background: #2e3035!important");
-  document.getElementById("wrapper_comp_based_courses2").className = "tac-dashboard-box dashboard-box-black";
-  document.getElementById("wrapper_events_lecturers").className = "tac-dashboard-box dashboard-box-black";
-  // document.getElementsByClassName("ul.plannerItemsList li h3").setAttribute("style", "color: #64686a"); 
-  // document.getElementsByClassName("classScheduleContainer").setAttribute("style", "filter: grayscale(100%)"); 
-  document.getElementsByClass("ul.plannerItemsList li h3")[0].style.color = "#ffffff";
-}
-
-
-var x = new MutationObserver(function(e) {
-  var dashboardWidgets = document.getElementById('wrapper_events_lecturers');
-  if (dashboardWidgets.addedNodes) {
-    console.log('Node Added');
-	setBlack();
-  }
-  if (!x) {
-    console.log('Node not detected');
-  }
-});
-
 function detectWidget(widgetName) {
   var dashboardWidgets = document.getElementById(widgetName);
   if (!dashboardWidgets) {
     //node does not exist yet
-    //wait 500ms and try again
+    //wait 1ms and try again
     //console.log(widgetName +': Not loaded');
     window.setTimeout(function() {
 		detectWidget(widgetName);
@@ -86,8 +47,6 @@ function detectWidget(widgetName) {
   setBlack(widgetName);
   console.log(widgetName +': Theme set');
 }
-
-
 
 function plannerText() {
   var x = document.getElementsByTagName("p");
@@ -104,11 +63,3 @@ function calendar() {
     x[i].style.color = "#212329";
   }
 }
-
-// document.getElementById("creditClassWrapper").className = "tac-dashboard-box dashboard-box-black";
-// userDashboardWidget.changeWidgetBackGround('creditClassWrapper','dashboard-box-black',this,'creditbasedClassesSelectedColorIcon'); 
-// userDashboardWidget.changeWidgetBackGround('studentSubmissionWrapper','dashboard-box-black',this,'studentSubmissionSelectedColorIcon'); 
-// userDashboardWidget.changeWidgetBackGround('studentGradebookWrapper','dashboard-box-black',this,'studentGradebookSelectedColorIcon'); 
-// userDashboardWidget.changeWidgetBackGround('wrapper_campus_communities','dashboard-box-black',this,'groupWidgetSelectedColorIcon'); 
-// userDashboardWidget.changeWidgetBackGround('wrapper_events_lecturers','dashboard-box-black',this,'eventsWidgetSelectedColorIcon'); 
-// userDashboardWidget.changeWidgetBackGround('wrapper_comp_based_courses2','dashboard-box-black',this,'progressWidgetSelectedColorIcon');
