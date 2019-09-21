7ecd75819a245cb507bffd16e732d206c76a19bc
jadedkitty <jadedkittycat@gmail.com>
2019-09-20 23:07:25 -0700

    Added Enhanced Branding and cleaned up code, added to chrome webstore

--- src/lms_colors.js
+++ src/lms_colors.js
@@ -4,8 +4,27 @@ style.type = 'text/css';
 style.href = chrome.extension.getURL('lms.css');
 (document.head||document.documentElement).appendChild(style);
 
+function addChat() {
+	var chat = document.createElement('iframe');
+	chat.src = 'https://deadsimplechat.com/4oAlSsyKG'
+	chat.width = '100%';
+	chat.height = '280px';
+	chat.id = 'chatFrame';
+	document.getElementById('my-groupsListWrap').appendChild(chat);
+	//console.log('chat.contentWindow =', chat.contentWindow);
+}
+function removeElement(elementId) {
+    // Removes an element from the document
+    var element = document.getElementById(elementId);
+    element.parentNode.removeChild(element);
+	
+}
+function changeText(elementId, tag, text) {
+	var element = document.getElementById(elementId);
+	document.parentNode.getElementsByTagName(tag)[0].textContent = text;
+}
 var BackgroundColor = "#34363e";
-var logoURL = chrome.extension.getURL("logo.png");
+var logoURL = chrome.extension.getURL("logoBlank.png");
 function setBlack(widget) {
   document.getElementById(widget).className = "tac-dashboard-box dashboard-box-black";
 }
@@ -14,22 +33,23 @@ function setBlack(widget) {
 window.onload = function() {  // runs functions on page load for detecting widgets and setting dark colors
   main();
   // detecting widgets
-  detectWidget('wrapper_events_lecturers');
-  detectWidget('creditClassWrapper');
-  detectWidget('studentSubmissionWrapper');
-  detectWidget('studentGradebookWrapper');
-  detectWidget('wrapper_campus_communities');
-  detectWidget('wrapper_comp_based_courses2');
+  detectAndSetWidget('wrapper_events_lecturers');
+  detectAndSetWidget('creditClassWrapper');
+  detectAndSetWidget('studentSubmissionWrapper');
+  detectAndSetWidget('studentGradebookWrapper');
+  detectAndSetWidget('wrapper_campus_communities');
+  detectAndSetWidget('wrapper_comp_based_courses2');  
   navMenu();
   navMenuText();
+  
   //plannerText();
 }
 
 function main() {
   document.body.style.background = BackgroundColor;
   document.getElementById("headerId").style.background = "#434653";
-  document.getElementsByClassName('lc_navIcons dashboardIco')[0].setAttribute("style", 'background: url("https://i.imgur.com/RqBuY1S.png") no-repeat scroll 0 0 rgba(0,0,0,0)');
   document.getElementsByTagName('img')[0].src = logoURL;
+  document.getElementsByTagName('img')[0].id = ('logoImg');
   // https://i.imgur.com/n50MHX2.png
   document.getElementsByClassName('headerMast')[0].style.background = "#434653";
   document.getElementsByClassName('selected_role')[0].style.color = "#ffffff";
@@ -40,14 +60,14 @@ function main() {
   document.getElementById("selectedNavTitle").setAttribute("style", "color: #ffffff !important");
 }
 
-function detectWidget(widgetName) {
+function detectAndSetWidget(widgetName) {
   var dashboardWidgets = document.getElementById(widgetName);
   if (!dashboardWidgets) {
     //node does not exist yet
     //wait 1ms and try again
     //console.log(widgetName +': Not loaded');
     window.setTimeout(function() {
-		detectWidget(widgetName);
+		detectAndSetWidget(widgetName);
 	}, 1);
     //console.log(widgetName +': Trying again...');
     return;
@@ -56,42 +76,28 @@ function detectWidget(widgetName) {
   //console.log(widgetName +': Setting theme...');
   setBlack(widgetName);
   console.log(widgetName +': Theme set');
-  if (widgetName == 'studentSubmissionWrapper') {
-	plannerText();
+  if (widgetName == 'wrapper_campus_communities') {
+	removeElement('noLearningCommunityData');
+	addChat();
+	var textnodes = nativeSelector(),
+    _nv;
+for (var i = 0, len = textnodes.length; i<len; i++){
+    _nv = textnodes[i].nodeValue;
+    textnodes[i].nodeValue = _nv.replace('Communities','Chat');
+}
   }
 }
-
-function plannerText() {
-  //var x = document.getElementsByTagName("p");
-  var parent = document.getElementById('planner_accordion');
-  var x = parent.getElementsByTagName('h3');
-  //var x = document.querySelectorAll('p');
-  if (!x) {
-    //node does not exist yet
-    //wait 1ms and try again
-    console.log('Planner Text: Not loaded');
-    window.setTimeout(function() {
-		plannerText();
-	}, 1);
-    console.log('Trying again...');
-    return;
-  }
-  console.log('setting color');
-  var i;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.color = "#ffffff!important";
-	console.log('color set');
-  }
-}
-
-
-
-function calendar() {
-  var x = document.getElementsByClassName("ui-widget-content");
-  var i;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.color = "#212329";
-  }
+function nativeSelector() {
+    var elements = document.querySelectorAll("body, body *");
+    var results = [];
+    var child;
+    for(var i = 0; i < elements.length; i++) {
+        child = elements[i].childNodes[0];
+        if(elements[i].hasChildNodes() && child.nodeType == 3) {
+            results.push(child);
+        }
+    }
+    return results;
 }
 
 function navMenu() {
