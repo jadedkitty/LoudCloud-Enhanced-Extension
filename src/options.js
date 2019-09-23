1cb19bf49e1c4524637d5057abf29c1190e3cdf6
jadedkitty <jadedkittycat@gmail.com>
2019-09-23 08:46:33 -0700

    Code formatting and text color change
    Changed color of main text elements and reformatted code for cleanliness

--- src/options.js
+++ src/options.js
@@ -1,31 +1,34 @@
 var defaultState = "on";
 
 function loadOptions() {
-	var toggleState = chrome.storage.sync.get('toggleState', function(data) {
+  var toggleState = chrome.storage.sync.get('toggleState', function(data) {
 
-	// valid colors are red, blue, green and yellow
-	if (toggleState == undefined || (toggleState != "on" && toggleState != "off")) {
-		toggleState = defaultState;
-	}
+    // valid colors are red, blue, green and yellow
+    if (toggleState == undefined || (toggleState != "on" && toggleState != "off")) {
+      toggleState = defaultState;
+    }
 
-	var select = document.getElementById("switch");
-	for (var i = 0; i < select.children.length; i++) {
-		var child = select.children[i];
-			if (child.value == toggleState) {
-			child.selected = "true";
-			break;
-		}
-	}
-});
+    var select = document.getElementById("switch");
+    for (var i = 0; i < select.children.length; i++) {
+      var child = select.children[i];
+      if (child.value == toggleState) {
+        child.selected = "true";
+        break;
+      }
+    }
+  });
 }
 loadOptions()
+
 function saveOptions() {
-	var select = document.getElementById("switch");
-	var color = select.children[select.selectedIndex].value;
-	chrome.storage.sync.set({ toggleState: switch });
+  var select = document.getElementById("switch");
+  var color = select.children[select.selectedIndex].value;
+  chrome.storage.sync.set({
+    toggleState: switch
+  });
 }
 
 function eraseOptions() {
-	localStorage.removeItem("toggleState");
-	location.reload();
+  localStorage.removeItem("toggleState");
+  location.reload();
 }
